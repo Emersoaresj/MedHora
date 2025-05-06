86562c1ac091da1a23366e92bff27d2a5cb4c7b9
Emerson Soares <emersonsoares269@icloud.com>
2025-05-06 20:59:55 -0300

    Projeto inicial do backend MedHora

---
+++ src/main/java/br/com/fiap/medhora/MedhoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medhora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedhoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedhoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/medhora/MedhoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medhora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedhoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
